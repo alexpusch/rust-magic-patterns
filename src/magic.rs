4f0d0cf0142adccd717ce2b11f55fcab660604b3
Alex Puschinsky <alexpoo@gmail.com>
2022-09-25 22:11:46 +0300

    Add example

---
+++ src/magic.rs
@@ -0,0 +1,62 @@
+#[derive(Clone)]
+pub struct Context {
+    param: String,
+    id: u32,
+}
+
+impl Context {
+    pub fn new(param: String, id: u32) -> Self {
+        Context { param, id }
+    }
+}
+pub struct Param(pub String);
+
+pub struct Id(pub u32);
+
+pub trait Handler<T> {
+    fn call(self, context: Context);
+}
+
+impl<F, T> Handler<T> for F
+where
+    F: Fn(T),
+    T: FromContext,
+{
+    fn call(self, context: Context) {
+        (self)(T::from_context(context));
+    }
+}
+
+impl<T1, T2, F> Handler<(T1, T2)> for F
+where
+    F: Fn(T1, T2),
+    T1: FromContext,
+    T2: FromContext,
+{
+    fn call(self, context: Context) {
+        (self)(T1::from_context(context.clone()), T2::from_context(context));
+    }
+}
+
+pub trait FromContext {
+    fn from_context(context: Context) -> Self;
+}
+
+impl FromContext for Param {
+    fn from_context(context: Context) -> Self {
+        Param(context.param)
+    }
+}
+
+impl FromContext for Id {
+    fn from_context(context: Context) -> Self {
+        Id(context.id)
+    }
+}
+
+pub fn trigger<T, H>(context: Context, handler: H)
+where
+    H: Handler<T>,
+{
+    handler.call(context);
+}
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use magic::{trigger, Context, Id, Param};
+
+mod magic;
+
+fn print_id(id: Id) {
+    println!("id is {}", id.0);
+}
+
+fn print_param(Param(param): Param) {
+    println!("param is {param}");
+}
+
+fn print_all(Param(param): Param, Id(id): Id) {
+    println!("param is {param}, id is {id}");
+}
+
+fn print_all_switched(Id(id): Id, Param(param): Param) {
+    println!("param is {param}, id is {id}");
+}
+
+pub fn main() {
+    let context = Context::new("test".into(), 33);
+
+    trigger(context.clone(), print_id);
+    trigger(context.clone(), print_param);
+    trigger(context.clone(), print_all);
+    trigger(context, print_all_switched);
+}
